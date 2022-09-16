995f05937c03aca532181c826b7ce9c304b910c2
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-16 16:12:50 +0300

    create-js-file

---
+++ script.js
@@ -0,0 +1,20 @@
+const table = document.getElementById('table');
+const timer = document.querySelector('.timer');
+const counter = document.querySelector('.moves');
+const restartBtn = document.querySelector('.restart');
+
+const cards = [{
+    name: "javascript",
+    image: "assets/javascript.svg",
+},
+{
+    name: "php",
+    image: "assets/php.svg",
+},
+{
+    name: "sql",
+    image: "assets/sql.svg",
+}];
+
+
+console.log(cards);
