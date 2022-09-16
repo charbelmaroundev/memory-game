dff46d8eee189eabc98194a4568b360a46293d04
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-16 16:29:46 +0300

    add-shuffle-card

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const table = document.getElementById('table');
+const tableEl = document.getElementById('table');
 const timer = document.querySelector('.timer');
-const counter = document.querySelector('.moves');
+const movesEl = document.querySelector('.moves');
 const restartBtn = document.querySelector('.restart');
+let firstSelection, secondSelection, selections, moves;
+let second;
 
 const cards = [{
     name: "javascript",
@@ -17,4 +19,41 @@ const cards = [{
 }];
 
 
-console.log(cards);
+const shuffle = () => {
+    let cardSlice = cards.slice(0, 3);
+    let fullSet = cardSlice.concat(cardSlice).sort(() => {
+        return 0.5 - Math.random();
+    });
+
+    fullSet.forEach(item => {
+        const name = item.name;
+        const image = item.image;
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.name = name;
+
+        const retro = document.createElement('div');
+        retro.classList.add('retro');
+
+        const front = document.createElement('div');
+        front.classList.add('front');
+        front.style.backgroundImage = 'url(' + image + ')';
+
+        tableEl.appendChild(card);
+        card.appendChild(front);
+        card.appendChild(retro);
+    });
+}
+
+const startGame = () => {
+    firstSelection = '';
+    secondSelection = '';
+    selections = 0;
+    moves = 0;
+    second = 0;
+    moves.textContent = 0;
+    timer.textContent = " 0 secs";
+    shuffle();
+}
+startGame();
